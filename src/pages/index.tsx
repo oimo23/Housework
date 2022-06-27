2cf1c27439574a585dcc38ec6e2239e6e08a5770
oimo23 <oimo2338@gmail.com>
2022-06-27 21:56:50 +0900

    不要ファイル削除

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,9 +13,13 @@ const Home: NextPage = () => {
   useEffect(() => {
     ;(async function () {
       try {
+        const params = {
+          //任意のパラメータ
+          crossDomain: true,
+        }
         const response: AxiosResponse<any> = await axios.get(
           `https://script.google.com/macros/s/AKfycbzxq3mEdG2fKxRAhY4eLABINIsZ7cyDKtMeFoTyS1T0Vl1U9nYzcibo4LkPsEvuKR7C/exec`,
-          { headers: { Authorization: 'Bearer token' } },
+          { headers: { Authorization: 'Bearer token' }, params },
         )
         setData(response.data)
       } catch (e) {
