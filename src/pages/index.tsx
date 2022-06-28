6972054a072985b8caf9172ef8f3c26c6ad56d67
oimo23 <oimo2338@gmail.com>
2022-06-29 00:32:44 +0900

    朝ごはん表示まで

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,58 +1,79 @@
 // import axios, { AxiosResponse } from 'axios'
-// import { Chart, registerables } from 'chart.js'
 import axios, { AxiosResponse } from 'axios'
+import {
+  ArcElement,
+  BarElement,
+  CategoryScale,
+  Chart,
+  Legend,
+  LinearScale,
+  Title,
+  Tooltip,
+} from 'chart.js';
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import React, { useEffect, useState } from 'react'
+import { Bar, Doughnut } from 'react-chartjs-2'
 
-// import { Bar } from 'react-chartjs-2'
 import Header from '../components/templates/Header'
 import styles from '../styles/Home.module.scss'
+Chart.register(
+  ArcElement,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
 
-// const chartData = {
-//   // x 軸のラベル
-//   labels: ['1 月', '2 月', '3 月', '4 月', '5 月', '6 月', '7 月'],
-//   datasets: [
-//     {
-//       label: 'Dataset',
-//       // データの値
-//       data: [65, 59, 80, 81, 56, 55, 40],
-//       // グラフの背景色
-//       backgroundColor: [
-//         'rgba(255, 99, 132, 0.2)',
-//         'rgba(255, 159, 64, 0.2)',
-//         'rgba(255, 205, 86, 0.2)',
-//         'rgba(75, 192, 192, 0.2)',
-//         'rgba(54, 162, 235, 0.2)',
-//         'rgba(153, 102, 255, 0.2)',
-//         'rgba(201, 203, 207, 0.2)',
-//       ],
-//       // グラフの枠線の色
-//       borderColor: [
-//         'rgb(255, 99, 132)',
-//         'rgb(255, 159, 64)',
-//         'rgb(255, 205, 86)',
-//         'rgb(75, 192, 192)',
-//         'rgb(54, 162, 235)',
-//         'rgb(153, 102, 255)',
-//         'rgb(201, 203, 207)',
-//       ],
-//       // グラフの枠線の太さ
-//       borderWidth: 1,
-//     },
-//   ],
-// }
+const chartData = {
+  labels: ['1 月', '2 月', '3 月', '4 月', '5 月', '6 月', '7 月'],
+  datasets: [
+    {
+      label: 'Dataset',
+      // データの値
+      data: [65, 59, 80, 81, 56, 55, 40],
+      // グラフの背景色
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(255, 159, 64, 0.2)',
+        'rgba(255, 205, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(201, 203, 207, 0.2)',
+      ],
+      // グラフの枠線の色
+      borderColor: [
+        'rgb(255, 99, 132)',
+        'rgb(255, 159, 64)',
+        'rgb(255, 205, 86)',
+        'rgb(75, 192, 192)',
+        'rgb(54, 162, 235)',
+        'rgb(153, 102, 255)',
+        'rgb(201, 203, 207)',
+      ],
+      // グラフの枠線の太さ
+      borderWidth: 1,
+    },
+  ],
+}
 
 const Home: NextPage = () => {
-  const [data, setData] = useState<any>()
+  const [yukiData, setYukiData] = useState<any>()
+  const [rinData, setRinData] = useState<any>()
+  const [breakFast, setBreakFast] = useState<any>()
 
   useEffect(() => {
     ;(async function () {
       try {
         const response: AxiosResponse<any> = await axios.get(
-          `https://script.google.com/macros/s/AKfycbxeb5KLPYAynSH1YeuQ8fkaKkZHpxJpF_xnkZevxFaVzF7ngSFy1a7rUKperYpMIZx0/exec`
-        )
-        setData(response.data)
+          `https://script.google.com/macros/s/AKfycbzHlX1OUKnGlVjrKgaFHPQgxRZFTU5IQRhVX-UGgRvplS8FYzeHtVeuZtxPSf8Bp5E/exec`,
+          { crossDomain: true }
+          )
+        setYukiData(response.data[0])
+        setRinData(response.data[1])
       } catch (e) {
         console.error(e)
       }
@@ -60,14 +81,28 @@ const Home: NextPage = () => {
   }, [])
 
   useEffect(() => {
-    console.info(data) 
-  }, [data])
+    const {breakFast} = aggrigate(yukiData, rinData)
+    setBreakFast(breakFast)
+    console.info(breakFast) 
+  }, [rinData])
 
-  const dataList = data ? data.map((d) => (
+  const yukiDataList = yukiData ? yukiData.map((d) => (
     <li key={d.date}>
       {d.date}
     </li>
   )) : null
+
+  const doughnutData1 = {
+    datasets: [
+      {
+        data: [breakFast?.yuki, breakFast?.rin],
+        backgroundColor: ['rgba(255, 99, 132, 0.2)', 'rgba(75, 192, 192, 0.2)'],
+        borderColor: ['rgb(255, 99, 132)', 'rgb(75, 192, 192)']
+      }
+    ],
+    labels: ['YUKI', 'RIN'],
+  }
+  
 
   return (
     <div className={styles.container}>
@@ -79,11 +114,47 @@ const Home: NextPage = () => {
 
       <Header />
       <main>
-        <ul>{dataList}</ul>
-        {/* <Bar data={chartData} /> */}
+        <div>
+          <h3 className="text-center">朝ごはん</h3>
+          <Doughnut data={doughnutData1} />
+        </div>
+        <Bar data={chartData} />
       </main>
     </div>
   )
+}
+
+const aggrigate = (yukiData: any, rinData: any) => {
+  const breakFast = {
+    yuki: 0,
+    rin: 0
+  }
+
+  if (!yukiData) return {breakFast}
+
+  console.info(yukiData)
+  console.info(rinData)
+
+  for(const dailyData of yukiData) {
+    if (dailyData.breakFast === '○') {
+      breakFast.yuki++
+    }
+  }
+
+  for(const dailyData of rinData) {
+    if (dailyData.breakFast === '○') {
+      breakFast.rin++
+    }
+  }
+
+  console.info(breakFast)
+
+  // 朝ごはんの100分率
+  // const breakFastTotal = breakFast.yuki + breakFast.rin
+  // breakFast.yuki = Math.floor(breakFast.yuki / breakFastTotal) * 100
+  // breakFast.rin  = 100 - breakFast.yuki
+
+  return {breakFast}
 }
 
 export default Home
